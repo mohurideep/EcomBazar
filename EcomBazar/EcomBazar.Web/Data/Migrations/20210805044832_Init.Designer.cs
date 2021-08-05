d68b2b614e2192b7f623df96ef58567bb7a00c09
Deep Mohuri <deepdeepmohuri@outlook.com>
2021-08-05 10:20:35 +0530

    Updating dbmigration

--- EcomBazar/EcomBazar.Web/Data/Migrations/20210805044832_Init.Designer.cs
+++ EcomBazar/EcomBazar.Web/Data/Migrations/20210805044832_Init.Designer.cs
@@ -4,14 +4,16 @@ using EcomBazar.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EcomBazar.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210805044832_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
