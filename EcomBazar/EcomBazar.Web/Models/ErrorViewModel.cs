59e328ca6cfd531bbb34b33f3ec3386efdf74150
Deep Mohuri <deepdeepmohuri@outlook.com>
2021-08-05 09:40:13 +0530

    First Commit

---
+++ EcomBazar/EcomBazar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcomBazar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
