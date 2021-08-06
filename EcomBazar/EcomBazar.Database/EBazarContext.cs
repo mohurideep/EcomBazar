063d5fc9c1f9c891b41c812f0c7e08fa6b53712f
Deep Mohuri <deepdeepmohuri@outlook.com>
2021-08-06 18:11:55 +0530

    Created catagory & Product entity and added db migration

---
+++ EcomBazar/EcomBazar.Database/EBazarContext.cs
@@ -0,0 +1,18 @@
+﻿using EcomBazar.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace EcomBazar.Database
+{
+    public class EBazarContext:DbContext
+    {
+        public EBazarContext():base("EcomBazarConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> products { get; set; }
+    }
+}
---
+++ EcomBazar/EcomBazar.Entity/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcomBazar.Entity
+{
+    public class Product: BaseEntity
+    {
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
